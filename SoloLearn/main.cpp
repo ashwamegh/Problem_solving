f8084fefc9cfa416174ab9dc7619671cdeed0271
shashank7200 <shashank7200@gmail.com>
2017-02-16 17:31:07 +0530

    Just Solo :rocket: :sos:

--- SoloLearn/main.cpp
+++ SoloLearn/main.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
+#include <math.h>
+#include <string.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+string s;
+cin>>s;
+char chaArr[s.length()];
+strcpy(chaArr,s.c_str());
+
+  if (s.length()%2==0){
+    for (int i = 0, j = s.length()-1; i< (s.length()/2), j>(s.length()/2)+ 1; i++, j--){
+             if(i==(s.length()/2)-1 && j==s.length()/2 && chaArr[i]==chaArr[j]){
+
+                    cout<<"YES"<<endl;
+            }
+
+            else if (chaArr[i] == chaArr[j]){
+                i++;
+                j--;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+
+    }
+
+  }
+
+  else{
+    for (int i=0, j=s.length()-1; i< (s.length()/2)+1,j>(s.length()/2)- 1; i++,j--){
+            if(i==(s.length()/2) && j==s.length()/2 && chaArr[i]==chaArr[j]){
+
+                    cout<<"YES"<<endl;
+            }
+            else if (chaArr[i] == chaArr[j]){
+                i++;
+                j--;
+            }
+
+            else{
+                cout<<"NO"<<endl;
+                break;
+            }
+
+    }
+  }
+
+return 0;
 }
