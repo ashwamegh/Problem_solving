dbca4769035a9d404264d46fe4459e8bcc1e450b
shashank7200 <shashank7200@gmail.com>
2017-06-17 17:55:35 +0530

    solved cash register problem

--- FCC/Advanced-Algorithm-Scripting/Exact-change.js
+++ FCC/Advanced-Algorithm-Scripting/Exact-change.js
@@ -1,23 +1,51 @@
+function checkCashRegister(price, cash, cid) {
+  let currencies = [
+    [ 'ONE HUNDRED', 100 ],
+  [ 'TWENTY', 20 ],
+  [ 'TEN', 10 ],
+  [ 'FIVE', 5 ],
+  [ 'ONE', 1 ],
+  [ 'QUARTER', 0.25 ],
+  [ 'DIME', 0.1 ],
+  [ 'NICKEL', 0.05 ],
+  [ 'PENNY', 0.01 ] 
+];
+  
+  let change = cash - price,
+  totalcid = cid.map((val) => val[1]).reduce((accm, val) => accm+val);
 
-function checkCashRegister(price, cashPay, cid) {
-  let change = cashPay - price, cash_in_drawer = 0;
-
-  let cash_in_drawer_arr = cid.map((arr) => cash_in_drawer + arr[1]);
-  cash_in_drawer = cash_in_drawer_arr.reduce((accum, num) => accum+num);
-
-  if(cash_in_drawer > change){
-      console.log(change.toFixed(2));
-  }
-  else if(cash_in_drawer === change){
-    console.log("Closed");
-    return "Closed";
-  }
-  else if(cash_in_drawer < change || cash_in_drawer_arr.indexOf(change)===-1){
-      console.log("Insufficient Funds");
+  if( totalcid < change){
       return "Insufficient Funds";
   }
+  else if( totalcid === change){
+      return "Closed";
+  }
 
-  return change;
+  cid = cid.reverse();
+
+  let exchangeValue = currencies.reduce((accum, next, index) => {
+
+      if ( change >= next[1]){
+          let currentValue = 0.0;
+
+          while( change >= next[1] && cid[index][1] >= next[1]){
+              currentValue += next[1];
+              change -= next[1];
+              change = Math.round(change * 100)/100;
+
+              cid[index][1] -= next[1];
+          }
+
+          accum.push([next[0], currentValue]);
+          return accum;
+      }
+      else{
+          return accum;
+      }
+  }, []);
+  console.log(change);
+  return exchangeValue.length>0 && change === 0? exchangeValue: "Insufficient Value";
 }
 
-checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+
+checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1.00], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
