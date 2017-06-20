4751fdbde532852114c406d62207e112d834b9d1
shashank7200 <shashank7200@gmail.com>
2017-06-21 00:17:41 +0530

    completed stack implementation using ES6

--- js-data-structures-algorithms-book/Chapter-3-Stacks/stacks-es6.js
+++ js-data-structures-algorithms-book/Chapter-3-Stacks/stacks-es6.js
@@ -1,40 +1,47 @@
 class Stack {
 
 	constructor(){
-		let items = [];
-
-		this.isEmpty = this.isEmpty.bind(this);
+		this.items = [];
 	}
 
 	push(element){
-		 return items.push(element);
+		 return this.items.push(element);
 	}
 
 	pop(){
-		return items.pop();
+		return this.items.pop();
 	}
 
 	peek() {
-		return items[items.length - 1];
+		return this.items[this.items.length - 1];
 	}
 
 	isEmpty(){
-		return items.length === 0;
+		return this.items.length === 0;
 	}
 
 	clear(){
-		return items=[];
+		return this.items=[];
 	}
 
 	size(){
-		return items.length;
+		return this.items.length;
 	}
 
 	print(){
-		 console.log(items.toString());
+		 return console.log(this.items.toString());
 	}
 }
 
 let stack = new Stack();
+stack.push(3);
+stack.push(4);
+stack.push(2);
+stack.push(45);
+stack.push(23);
+stack.push(91);
+stack.push(01);
+stack.pop();
 
+stack.print();
 console.log(stack.isEmpty());
