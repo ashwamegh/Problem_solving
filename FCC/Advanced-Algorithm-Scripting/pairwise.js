02c4882905b8de1f1fa9378976ee7d94ec353d33
Twinkle Sinha <Twinkle Sinha>
2017-07-21 13:00:40 +0530

    solved pairwise algo

--- FCC/Advanced-Algorithm-Scripting/pairwise.js
+++ FCC/Advanced-Algorithm-Scripting/pairwise.js
@@ -1,6 +1,19 @@
-
+// https://www.freecodecamp.org/challenges/pairwise
 function pairwise(arr, arg) {
-  return arg;
+  let count = 0;
+  let usedIndices = [];
+
+  for(let i=0; i<arr.length-1; i++){
+    for(let j=i+1; j<arr.length; j++){
+      if(arr[i]+arr[j] === arg && usedIndices.indexOf(i)===-1 && usedIndices.indexOf(j)===-1){
+        count+=i+j;
+        usedIndices.push(i);
+        usedIndices.push(j);
+      }
+    }
+  }
+  console.log(count);
+  return count;
 }
 
-pairwise([1,4,2,3,0,5], 7);
+pairwise([0, 0, 0, 0, 1, 1], 1);
