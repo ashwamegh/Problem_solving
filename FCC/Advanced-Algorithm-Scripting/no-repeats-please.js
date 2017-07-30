e59e2bae2fbf211f5d91fabe2796b08ce49196d4
shashank7200 <shashank7200@gmail.com>
2017-07-31 00:30:49 +0530

    hardly :eyeglasses: completed no repeats please algo

--- FCC/Advanced-Algorithm-Scripting/no-repeats-please.js
+++ FCC/Advanced-Algorithm-Scripting/no-repeats-please.js
@@ -1,6 +1,29 @@
+// https://www.freecodecamp.com/challenges/no-repeats-please
+
+// Uses Heaps Algorithm : https://en.wikipedia.org/wiki/Heap%27s_algorithm
 
 function permAlone(str) {
-  return str;
-}
+	let strArray = str.split('');
+	let result = 0;
 
+	const swapping = (a,b) => {
+		let temp = strArray[a];
+		strArray[a] = strArray[b];
+		strArray[b] = temp;
+	}
+
+	const permutationsMatch= (length)=>{
+		let regex = /([a-z])\1+/;
+		if (length === 1 && !regex.test(strArray.join(''))){
+			result++;
+		}else{
+				for (let i = 0; i < length; i++) {
+					permutationsMatch(length-1);
+					swapping(length%2?0:i,length-1);
+				}
+			}
+	}
+	permutationsMatch(strArray.length);
+	console.log(result);
+}
 permAlone('aab');
